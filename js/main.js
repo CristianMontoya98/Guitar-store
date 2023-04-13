2d4c79c3732a74fe0f323cee4fb96497068bd660
Cristian Montoya <chrimonlo@gmail.com>
2023-04-12 20:14:38 -0500

    Event listener code factored into a function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const button1 = document.querySelector('#button1')
-        const modal = document.querySelector('.modal')
-        const close = document.querySelector('.modal__content--close')
-        button1.addEventListener('click', () => {
-            modal.classList.remove('hidden')
-            modal.classList.add('visible')
-        })
-        close.addEventListener('click', () => {
-            modal.classList.add('hidden')
-            modal.classList.remove('visible')
-        })
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.modal__content--close');
+
+
+const addListener = (element, close) => {
+  element.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+  });
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    modal.classList.remove('visible')
+  })
+}
+
+addListener(button1, closeButton);
+addListener(button2, closeButton);
+addListener(button3, closeButton);
